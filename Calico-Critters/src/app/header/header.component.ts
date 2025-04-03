ba620d7c97dd23c48ffa7630788e419893590ac0
Rebecca Baldwin <baldwin2@colostate.edu>
2025-04-02 18:47:00 -0600

    added navbar functions and added about back and etc

--- Calico-Critters/src/app/header/header.component.ts
+++ Calico-Critters/src/app/header/header.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  items = [
+   { routeLink: 'homepage', label: 'Homepage'},
+   { routeLink: 'about', label: 'About'},
+   { routeLink: 'color', label: 'Color Generator'},
+  ]
 
 }
